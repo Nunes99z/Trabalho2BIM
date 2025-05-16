205b51764c9d9e27c56c0034835f13f85be15eb0
unknown <tallytha.preciliano@eaportal.org>
2025-05-16 12:18:51 -0300

    salvamento 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,22 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(1)
+  const [texto, setTexto] = useState("")
   const handleClick = () => {
     setCount(count * 2)
     console.log(count)
+  }
+  const handleTexto = (e) => {
+    console.log(e.target.value)
+      setTexto(e.target)
   }
 
   return (
     <>
       <div className="card">
+        <input type="text" placeholder='digite here' onChange={handleTexto} />
         <button onClick={handleClick}>Clique Aqui</button>
-        {count}
+        {texto}
       </div>
     </>
   )
