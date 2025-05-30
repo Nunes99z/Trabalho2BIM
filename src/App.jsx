48b01a096d50edeb49291ce9c18693489d0d31aa
unknown <lucas.nunes@eaportal.org>
2025-05-30 12:29:01 -0300

    salvar

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,16 @@ import { useState } from "react";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(1)
-  const [texto, setTexto] = useState("")
+  const [nome, setNome] = useState("")
+  const [dia, setDia] = useState('')
+  const [desc, setDesc] = useState('')
+  const [materia, setMateria] = useState('')
+  const [filtro, setFiltro] = useState('')
+
+  const adicionarTarefas = () => {
+    if (nome && desc && dia )
+  }
+
   const handleClick = () => {
     setCount(count * 2)
     console.log(count)
@@ -16,7 +24,7 @@ function App() {
   return (
     <>
       <div className="card">
-        <input type="text" placeholder='digite here' onChange={handleTexto} />
+        <input type="text" placeholder='digite a tarefa' onChange={handleTexto} />
         <button onClick={handleClick}>Clique Aqui</button>
         {texto}
       </div>
